e79435e11276bcf1cf54e880611d4227b1f0576a
BarsTigerMeowcat <kit.barsic@gmail.com>
2020-03-12 18:34:54 +0200

    1

--- Zverushka/ProstoZverushka.py
+++ Zverushka/ProstoZverushka.py
@@ -1,3 +1,7 @@
 class Critter(object):
     def talk(self):
         print("Привет. Я - зверюшка - экземпляр класса Critter.")
+
+crit = Critter()
+crit.talk()
+input("\n\nНажмите Ent, чтобы выйти")
